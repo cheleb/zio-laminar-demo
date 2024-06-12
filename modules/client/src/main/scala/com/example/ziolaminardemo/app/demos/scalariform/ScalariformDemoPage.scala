ab3efac80f56161358a74c79487858b4f0ab528e
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-06-12 23:03:07 +0200

    chore: Update ScalariformDemoPage to include userBus and renderUser function
    The code changes in ScalariformDemoPage include the addition of the userBus variable and the renderUser function. The userBus is an EventBus that emits User events, and the renderUser function renders the User data on the page. This update enhances the functionality of the page by allowing the display of user information and handling user events.

--- modules/client/src/main/scala/com/example/ziolaminardemo/app/demos/scalariform/ScalariformDemoPage.scala
+++ modules/client/src/main/scala/com/example/ziolaminardemo/app/demos/scalariform/ScalariformDemoPage.scala
@@ -6,20 +6,43 @@ import dev.cheleb.scalamigen.{*, given}
 
 import com.example.ziolaminardemo.domain.*
 import com.example.ziolaminardemo.core.ZJS.*
+import be.doeraene.webcomponents.ui5.Button
 
 object ScalariformDemoPage:
   def apply() =
     val personVar = Var(Person("Alice", 42, Left(Cat("Fluffy"))))
+    val userBus   = EventBus[User]()
+
     div(
-      h1("Hello World"),
-      child.text <-- personVar.signal.map(p => s"$p"),
-      Form.renderVar(personVar),
-      hr(),
-      button(
-        "Post",
-        onClick --> { _ =>
-          println(personVar.now())
-          useBackend(_.person.createEndpoint(personVar.now())).runJs
-        }
+      styleAttr := "width: 100%; overflow: hidden;",
+      div(
+        styleAttr := "width: 600px; float: left;",
+        h1("Hello World"),
+        child.text <-- personVar.signal.map(p => s"$p"),
+        Form.renderVar(personVar)
+      ),
+      div(
+        styleAttr := "width: 60px; float: left; margin-top: 200px;",
+        Button(
+          "Post",
+          onClick --> { _ =>
+            println(personVar.now())
+            useBackend(_.person.createEndpoint(personVar.now())).emitTo(userBus)
+          }
+        )
+      ),
+      div(
+        styleAttr := "width: 600px; float: left;",
+        h1("Response"),
+        child <-- userBus.events.map(renderUser)
       )
+    )
+
+  def renderUser(user: User) =
+    div(
+      h2("User"),
+      div(s"Name: ${user.name}"),
+      div(s"Age: ${user.age}"),
+      div(s"Pet: ${user.pet}"),
+      div(s"Creation Date: ${user.creationDate}")
     )
