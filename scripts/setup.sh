88dcf59bc1f3e7ce561cc4c641e0f4e1247ce95a
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-09-26 00:26:02 +0200

    Refactor npm dependency installation logic in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,20 +6,24 @@ rm -f $NPM_DEV_STARTED
 
 filename_lock=node_modules/.package-lock.json
 
-function npmInstall() {
-    if [ ! -f "$filename_lock" ]; then
-        echo "First time setup: Installing npm dependencies..."
-        npm i
-    else
-        filename=package.json
-        age=$(($(date +%s) - $(stat -t %s -f %m -- "$filename")))
-        age_lock=$(($(date +%s) - $(stat -t %s -f %m -- "$filename_lock")))
-        if [ $age_lock -gt $age ]; then
-            echo "Reinstalling npm dependencies..."
-            npm i
-        fi
-    fi
+function npmInstall () {
+  if [ ! -f "$filename_lock" ]; then
+      echo "First time setup: Installing npm dependencies..."
+      npm i
+  else
+      filename=package.json
+      age=$(($(date +%s) - $(stat -t %s -f %m -- "$filename")))
+      age_lock=$(($(date +%s) - $(stat -t %s -f %m -- "$filename_lock")))
+      echo "Reinstalling npm dependencies..."
+      if [ $age_lock -gt $age ]; then
+          echo "(New dependencies)"
+          npm i
+      else
+          npm ci
+      fi
+  fi
 }
+
 
 cd modules/client
 
